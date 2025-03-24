56dc24ed5431f74d06d5485182732452aa3fbed0
markx86 <25851052+markx86@users.noreply.github.com>
2025-03-24 22:15:55 +0100

    Improve build script for client

--- client/scripts/build.py
+++ client/scripts/build.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3
 
 import sys
-import os
 import subprocess
 import base64
 import re
+from os import makedirs
+from os.path import dirname, abspath
+from pathlib import Path
 
 release_mode = "--release" in sys.argv
 
-here_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
-src_dir  = os.path.join(here_dir, "..", "src")
-dist_dir = os.path.join(here_dir, "..", "dist")
-wasm_dir = os.path.join(src_dir, "wasm")
+here_dir = Path(abspath(dirname(sys.argv[0])))
+src_dir = here_dir / ".." / "src";
+dist_dir = here_dir / ".." / "dist";
+wasm_dir = src_dir / "wasm";
 
-os.makedirs(dist_dir, exist_ok=True)
+makedirs(dist_dir, exist_ok=True)
 
-subprocess.check_call(os.path.join(wasm_dir, "build.sh"), cwd=dist_dir)
+subprocess.check_call(wasm_dir / "build.sh", cwd=dist_dir)
 
-wasm = open(os.path.join(dist_dir, "gloom.wasm"), "rb").read()
+wasm = open(dist_dir / "gloom.wasm", "rb").read()
 wasm_b64 = base64.b64encode(wasm)
 
-app_js = open(os.path.join(src_dir, "app.js"), "rb").read()
+app_js = open(src_dir / "app.js", "rb").read()
 app_js = app_js.replace(b"@@WASMB64@@", wasm_b64)
 
 if release_mode:
@@ -28,11 +30,11 @@ if release_mode:
 else:
     app_min_js = app_js
 
-html = open(os.path.join(src_dir, "app.html"), "r").read()
+html = open(src_dir / "app.html", "r").read()
 if release_mode:
     html_min = re.sub(r">\s+<", "><", html).encode()
 else:
     html_min = html.encode()
 html_min = html_min.replace(b"@@JAVASCRIPT@@", app_min_js)
 
-open(os.path.join(dist_dir, "index.html"), "wb").write(html_min)
+open(dist_dir / "index.html", "wb").write(html_min)
